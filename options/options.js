6aff920945136ed7d8a5430220f69d091e2c945e
Crowdbotics <team@crowdbotics.com>
2023-03-01 06:21:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-credit-39172",
+  url: "https://polished-credit-39172.botics.co",
+  api: "https://polished-credit-39172.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
